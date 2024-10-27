4e496334742d4cbc34470a4ce79a2683977efee9
Mkhaled00 <malduraywish24@twq.idserve.net>
2024-10-27 19:57:03 +0300

    Initial Commit

---
+++ recipes/recipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipesApp.swift
+//  recipes
+//
+//  Created by muntaha khaled Alduraywish on 27/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct recipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ recipesTests/recipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  recipesTests.swift
+//  recipesTests
+//
+//  Created by muntaha khaled Alduraywish on 27/10/2024.
+//
+
+import Testing
+@testable import recipes
+
+struct recipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
